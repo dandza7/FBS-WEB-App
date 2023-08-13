7967c41cf131ee81b9ec77b61f4cab8e3dc0c719
dandza7 <dane.m.12@gmail.com>
2023-08-13 21:10:27 +0200

    Added endpoint for one league

--- FBSApp/FBSApp/Services/ILeagueService.cs
+++ FBSApp/FBSApp/Services/ILeagueService.cs
@@ -5,6 +5,7 @@ namespace FBSApp.Services
     public interface ILeagueService
     {
         IEnumerable<LeagueDTO> GetAll();
+        LeagueDTO GetById(long id);
         long Create(NewLeagueDTO newLeague);
     }
 }
